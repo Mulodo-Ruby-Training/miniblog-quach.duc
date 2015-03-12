fd4e9f68372f11c35a46658fb001e74a50a9c372
quach-duc-mulodo <quach.duc@mulodo.com>
2015-03-12 18:01:03 +0700

    Done feature create post (screen)

--- src/frontend/app/controllers/posts_controller.rb
+++ src/frontend/app/controllers/posts_controller.rb
@@ -1,12 +1,65 @@
 class PostsController < ApplicationController
+   # Set funtions use partial funtion in private
+   before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # TODO: pagging, bootstap, filter (Ex: status, use_id,..), partial and helper
   def index
     # DESC===========================
     # Get all of Post to show in view
-    # TODO: pagging
-    #            bootstap
-    #            filter (Ex: status, use_id,..)
-    #            partial and helper
+    # OUT: all record of Post
     # DESC===========================
     @posts = Post.all
   end
+
+  def create
+    # DESC===========================
+    # Create record for Post
+    # IN: param_post
+    # OUT: create new record in database
+    #         show record in view
+    # DESC===========================
+    # Post.create(param_post)
+  end
+
+  def edit
+    # DESC===========================
+    # DESC===========================
+  end
+
+  def new
+    # DESC===========================
+    # Init new post for partial form
+    # IN:
+    # OUT: record empty for partial form
+    # DESC===========================
+    @post = Post.new
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_post
+      # DESC===========================
+      # Partial for funtions use (edit, delete)
+      # IN: params[:id]
+      # OUT: post have this id
+      # DESC===========================
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+        # DESC===========================
+        # Filter params to get Post's param
+        # IN: params
+        # OUT: post_params
+        # DESC===========================
+        params.require(:post).permit(:title, :description, :content, :thumbnail, :status, :user_id)
+    end
 end
--- src/frontend/db/seeds.rb
+++ src/frontend/db/seeds.rb
@@ -13,16 +13,16 @@
 # t.string         :thumbnail
 # t.integer       :user_id
 # t.boolean      :status
-# CREAT==========================
-# Post.create([
-#                       {title: 'Title 1', description: 'Decription 1', content: 'Content 1', thumbnail: 'thumbnail 1', user_id: '1', status: true },
-#                       {title: 'Title 2', description: 'Decription 2', content: 'Content 2', thumbnail: 'thumbnail 2', user_id: '1', status: true },
-#                       {title: 'Title 3', description: 'Decription 3', content: 'Content 3', thumbnail: 'thumbnail 3', user_id: '1', status: false },
-#                       {title: 'Title 3', description: 'Decription 4', content: 'Content 4', thumbnail: 'thumbnail 4', user_id: '1', status: false }
-#                  ])
 # DELETE=========================
-# Post.delete_all()
+Post.delete_all()
 # like
 # Rails console
 # ActiveRecord::Base.connection.execute("DELETE from posts")
+# CREAT==========================
+Post.create([
+                      {title: 'Title 1', description: 'Decription 1', content: 'Content 1', thumbnail: 'thumbnail 1', user_id: '1', status: true },
+                      {title: 'Title 2', description: 'Decription 2', content: 'Content 2', thumbnail: 'thumbnail 2', user_id: '1', status: true },
+                      {title: 'Title 3', description: 'Decription 3', content: 'Content 3', thumbnail: 'thumbnail 3', user_id: '1', status: false },
+                      {title: 'Title 3', description: 'Decription 4', content: 'Content 4', thumbnail: 'thumbnail 4', user_id: '1', status: false }
+                 ])
 # ==============================
