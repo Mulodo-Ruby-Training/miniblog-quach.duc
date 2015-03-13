d292fcec8a3d0a7a991ca6867e9fe3ca3a6280a6
quach-duc-mulodo <quach.duc@mulodo.com>
2015-03-13 10:56:37 +0700

    Finish feature delete Post

--- src/frontend/app/controllers/posts_controller.rb
+++ src/frontend/app/controllers/posts_controller.rb
@@ -52,6 +52,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+      respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
